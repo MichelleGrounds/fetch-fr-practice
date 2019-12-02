475f764ad959506b8f9445776e694b5b87e31104
Michelle Grounds <mischoel@googlemail.com>
2019-12-02 10:34:18 +0000

    initial commit-

---
+++ fetch-pokemon-fr/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./Components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <header>Fetch a Pokemon</header>
+      <br></br>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
